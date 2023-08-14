423c30bcbacda8f7534f6b4df3fc76806f9ffcbc
song <hellsxs@163.com>
2023-08-14 12:47:07 +0800

    删除字符使频率相同

---
+++ 2423.js
@@ -0,0 +1,51 @@
+/**
+ * 删除字符使频率相同
+ *
+ * 给你一个下标从 0 开始的字符串 word ，字符串只包含小写英文字母。你需要选择 一个 下标并 删除 下标处的字符，使得 word 中剩余每个字母出现 频率 相同。
+ * 如果删除一个字母后，word 中剩余所有字母的出现频率都相同，那么返回 true ，否则返回 false 。
+ * 注意：
+ * 字母 x 的 频率 是这个字母在字符串中出现的次数。
+ * 你 必须 恰好删除一个字母，不能一个字母都不删除。
+ *
+ * 示例 1：
+ * 输入：word = "abcc"
+ * 输出：true
+ * 解释：选择下标 3 并删除该字母，word 变成 "abc" 且每个字母出现频率都为 1 。
+ *
+ * 示例 2：
+ * 输入：word = "aazz"
+ * 输出：false
+ * 解释：我们必须删除一个字母，所以要么 "a" 的频率变为 1 且 "z" 的频率为 2 ，要么两个字母频率反过来。所以不可能让剩余所有字母出现频率相同。
+ *
+ * 提示：
+ * 2 <= word.length <= 100
+ * word 只包含小写英文字母。
+ */
+
+/**
+ * @param {string} word
+ * @return {boolean}
+ */
+var equalFrequency = function (word) {
+  // 每种 出现频率
+  const list1 = new Map()
+  for (const e of word) {
+    list1.set(e, (list1.get(e) || 0) + 1)
+  }
+  if (list1.size === 1) return true
+
+  // 每种 频率 减一 看频率是否相同
+  for (let [val] of [...new Set([...word])]) {
+    const list2 = new Set()
+    list1.forEach((e, key) => {
+      if ((key === val && --e) || key !== val) {
+        list2.add(e)
+      }
+    })
+    if (list2.size === 1) return true
+  }
+  return false
+}
+
+console.log(equalFrequency('abcc'))
+console.log(equalFrequency('aazz'))
