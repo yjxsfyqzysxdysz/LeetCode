efbc4ca09953abb5d1aa8e1467614d5ea19c8bae
song <hellsxs@163.com>
2023-08-25 22:08:06 +0800

    删除排序数组中的重复项

--- 26.js
+++ 26.js
@@ -1,55 +1,73 @@
 /**
- * 删除排序数组中的重复项
+ * 26. 删除排序数组中的重复项
  *
- * 给定一个排序数组，你需要在 原地 删除重复出现的元素，使得每个元素只出现一次，返回移除后数组的新长度。
- * 不要使用额外的数组空间，你必须在 原地 修改输入数组 并在使用 O(1) 额外空间的条件下完成。
+ * 给你一个 升序排列 的数组 nums ，请你 原地 删除重复出现的元素，使每个元素 只出现一次 ，返回删除后数组的新长度。元素的 相对顺序 应该保持 一致 。然后返回 nums 中唯一元素的个数。
+ * 考虑 nums 的唯一元素的数量为 k ，你需要做以下事情确保你的题解可以被通过：
+ * 更改数组 nums ，使 nums 的前 k 个元素包含唯一元素，并按照它们最初在 nums 中出现的顺序排列。nums 的其余元素与 nums 的大小不重要。
+ * 返回 k 。
  *
- * 示例 1:
- * 给定数组 nums = [1,1,2],
- * 函数应该返回新的长度 2, 并且原数组 nums 的前两个元素被修改为 1, 2。
- * 你不需要考虑数组中超出新长度后面的元素。
- *
- * 示例 2:
- * 给定 nums = [0,0,1,1,1,2,2,3,3,4],
- * 函数应该返回新的长度 5, 并且原数组 nums 的前五个元素被修改为 0, 1, 2, 3, 4。
- * 你不需要考虑数组中超出新长度后面的元素。
- *
- * 说明:
- * 为什么返回数值是整数，但输出的答案是数组呢?
- * 请注意，输入数组是以「引用」方式传递的，这意味着在函数里修改输入数组对于调用者是可见的。
- *
- * 你可以想象内部操作如下:
- * // nums 是以“引用”方式传递的。也就是说，不对实参做任何拷贝
- * int len = removeDuplicates(nums);
- * // 在函数里修改输入数组对于调用者是可见的。
- * // 根据你的函数返回的长度, 它会打印出数组中该长度范围内的所有元素。
- * for (int i = 0; i < len; i++) {
- *     print(nums[i]);
+ * 判题标准:
+ * 系统会用下面的代码来测试你的题解:
+ * int[] nums = [...]; // 输入数组
+ * int[] expectedNums = [...]; // 长度正确的期望答案
+ * int k = removeDuplicates(nums); // 调用
+ * assert k == expectedNums.length;
+ * for (int i = 0; i < k; i++) {
+ *     assert nums[i] == expectedNums[i];
  * }
+ * 如果所有断言都通过，那么您的题解将被 通过。
+ *
+ * 示例 1：
+ * 输入：nums = [1,1,2]
+ * 输出：2, nums = [1,2,_]
+ * 解释：函数应该返回新的长度 2 ，并且原数组 nums 的前两个元素被修改为 1, 2 。不需要考虑数组中超出新长度后面的元素。
+ *
+ * 示例 2：
+ * 输入：nums = [0,0,1,1,1,2,2,3,3,4]
+ * 输出：5, nums = [0,1,2,3,4]
+ * 解释：函数应该返回新的长度 5 ， 并且原数组 nums 的前五个元素被修改为 0, 1, 2, 3, 4 。不需要考虑数组中超出新长度后面的元素。
+ *
+ *
+ * 提示：
+ * 1 <= nums.length <= 3 * 10^4
+ * -10^4 <= nums[i] <= 10^4
+ * nums 已按 升序 排列
  */
 
 /**
  * @param {number[]} nums
  * @return {number}
  */
-var removeDuplicates = function (nums) {
-  for (let i = 0, len = nums.length; i < len; i++) {
-    if (nums[i] === nums[i - 1]) {
-      nums.splice(i, 1)
-      len--
-      i--
-    }
-  }
-  return nums.length
-}
+// var removeDuplicates = function (nums) {
+//   for (let i = 0, len = nums.length; i < len; i++) {
+//     if (nums[i] === nums[i - 1]) {
+//       nums.splice(i, 1)
+//       len--
+//       i--
+//     }
+//   }
+//   return nums.length
+// }
+
+// var removeDuplicates = function (nums) {
+//   let index = 0
+//   for (let i = 1, len = nums.length; i < len; i++) {
+//     if (nums[i] !== nums[index]) {
+//       nums[++index] = nums[i]
+//     }
+//   }
+//   return index + 1
+// }
 
 var removeDuplicates = function (nums) {
-  let index = 0
-  for (let i = 1, len = nums.length; i < len; i++) {
-    if (nums[i] !== nums[index]) {
-      nums[++index] = nums[i]
+  let low = 0
+  for (let fast = 1, n = nums.length; fast < n; fast++) {
+    if (nums[low] !== nums[fast]) {
+      nums[++low] = nums[fast]
     }
   }
-  return index + 1
+  return low + 1
 }
+
+console.log(removeDuplicates([1, 1, 2]))
 console.log(removeDuplicates([0, 0, 1, 1, 1, 2, 2, 3, 3, 4]))
