4db759f7973d179c5dd1559f888916a8ecdcb54d
song <hellsxs@163.com>
2023-08-25 21:25:28 +0800

    杨辉三角 II

--- 119.js
+++ 119.js
@@ -1,4 +1,6 @@
 /**
+ * 119. 杨辉三角 II
+ *
  * 给定一个非负索引 rowIndex，返回「杨辉三角」的第 rowIndex 行。
  * 在「杨辉三角」中，每个数是它左上方和右上方的数的和。
  *
@@ -37,19 +39,19 @@
  * @param {number} rowIndex
  * @return {number[]}
  */
-var getRow = function (rowIndex) {
-  if (rowIndex === 0) return [1]
-  if (rowIndex === 1) return [1, 1]
-  const res = [[1], [1, 1]]
-  for (let i = 2; i <= rowIndex; i++) {
-    let tmp = []
-    for (let j = 1, arr = res[i - 1]; j < i; j++) {
-      tmp.push(arr[j] + arr[j - 1])
-    }
-    res.push([1, ...tmp, 1])
-  }
-  return res[rowIndex]
-}
+// var getRow = function (rowIndex) {
+//   if (rowIndex === 0) return [1]
+//   if (rowIndex === 1) return [1, 1]
+//   const res = [[1], [1, 1]]
+//   for (let i = 2; i <= rowIndex; i++) {
+//     let tmp = []
+//     for (let j = 1, arr = res[i - 1]; j < i; j++) {
+//       tmp.push(arr[j] + arr[j - 1])
+//     }
+//     res.push([1, ...tmp, 1])
+//   }
+//   return res[rowIndex]
+// }
 
 // var getRow = function (rowIndex) {
 //   let row = []
@@ -62,6 +64,17 @@ var getRow = function (rowIndex) {
 //   }
 //   return row[rowIndex]
 // }
-console.log(getRow(3))
+
+// 错位相加
+var getRow = function (rowIndex) {
+  let res = []
+  for (let i = 0; i <= rowIndex; i++) {
+    res = [1, ...res.map((e, i, arr) => e + (arr[i + 1] || 0))]
+  }
+  return res
+}
+
 console.log(getRow(0))
 console.log(getRow(1))
+console.log(getRow(2))
+console.log(getRow(3))
