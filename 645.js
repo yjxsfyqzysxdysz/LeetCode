ec911f6e14551afa62b0b39180b3b8576fa24a5a
song <hellsxs@163.com>
2023-08-31 12:05:54 +0800

    错误的集合

---
+++ 645.js
@@ -0,0 +1,40 @@
+/**
+ * 645. 错误的集合
+ *
+ * 集合 s 包含从 1 到 n 的整数。不幸的是，因为数据错误，导致集合里面某一个数字复制了成了集合里面的另外一个数字的值，导致集合 丢失了一个数字 并且 有一个数字重复 。
+ * 给定一个数组 nums 代表了集合 S 发生错误后的结果。
+ * 请你找出重复出现的整数，再找到丢失的整数，将它们以数组的形式返回。
+ *
+ * 示例 1：
+ * 输入：nums = [1,2,2,4]
+ * 输出：[2,3]
+ *
+ * 示例 2：
+ * 输入：nums = [1,1]
+ * 输出：[1,2]
+ *
+ * 提示：
+ * 2 <= nums.length <= 10^4
+ * 1 <= nums[i] <= 10^4
+ */
+
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var findErrorNums = function (nums) {
+  const n = nums.length
+  const arr = Array(n).fill(1)
+  for (const e of nums) {
+    arr[e - 1]--
+  }
+  const res = []
+  arr.forEach((e, i) => {
+    if (e == 1) res.push(i + 1)
+    else if (e == -1) res.unshift(i + 1)
+  })
+  return res
+}
+
+console.log(findErrorNums([1, 2, 2, 4]))
+console.log(findErrorNums([1, 1]))
